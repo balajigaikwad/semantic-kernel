46f49bdc96dd0c3d0fe1c8bd1844a5d87ca53d6d
Balaji <gaikwadbalaji9@gmail.com>
2026-02-24 19:17:50 +0530

    added memory to the chat

---
+++ SKMemory/PromptLoggingFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.SemanticKernel;
+
+public sealed class PromptLoggingFilter : IPromptRenderFilter
+{
+    public async Task OnPromptRenderAsync(PromptRenderContext context, Func<PromptRenderContext, Task> next)
+    {
+        // 1. Let the kernel finish rendering the template
+        await next(context);
+
+        // 2. Access the final string that will be sent to the LLM
+        string finalPrompt = context.RenderedPrompt;
+
+        Console.WriteLine("--- DEBUG: RENDERED PROMPT ---");
+        Console.WriteLine(finalPrompt);
+        Console.WriteLine("------------------------------");
+    }
+}
